48006fe0eb71e89018b25252f5a87d731fe884cb
lthai423 <lthai423@gmail.com>
2016-09-05 18:17:37 -0700

    Fixed 404.

--- server/routes.js
+++ server/routes.js
@@ -24,4 +24,20 @@ module.exports = function (app, express) {
         }
       })
     })
+
+  app.use(function(req, res, next){
+    res.status(404);
+
+    if (req.accepts('html')) {
+      // respond with html page
+      res.send('404 - Not found');
+      return;
+    } else if (req.accepts('json')) {
+      // respond with json
+      res.send({ error: '404 - Not found' });
+      return;
+    }
+    // default to plain-text. send()
+    res.type('txt').send('404 - Not found');
+  });
 };
