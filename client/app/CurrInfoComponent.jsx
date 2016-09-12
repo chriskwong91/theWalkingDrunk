b4ed5d91ed49323a32945602a8c83d10f650e61d
lthai423 <lthai423@gmail.com>
2016-09-12 10:48:01 -0700

    Refactored index.jsx and added Specs

--- client/app/CurrInfoComponent.jsx
+++ client/app/CurrInfoComponent.jsx
@@ -5,23 +5,28 @@ class CurrInfoComponent extends React.Component {
 		super(props);
 	}
 
+	componentDidUpdate(){
+		// if (this.props.current.photos)
+		// 	console.log(this.props.current.photos[0].getUrl())
+	}
+
 	render(){
 		return (
 			<div>
 				<div>
-					<b>Pub Name:</b>
+					<b>Pub Name: </b>
 					{this.props.current.name}
 				</div>
 				<div>
-					<b>Address:</b>
+					<b>Address: </b>
 					{this.props.current.vicinity}
 				</div>
 				<div>
-					<b>Rating:</b>
+					<b>Rating: </b>
 					{this.props.current.rating}
 				</div>
 				<div>
-					<b>Price Level:</b>
+					<b>Price Level: </b>
 					{this.props.current.price_level}
 				</div>
 			</div>
