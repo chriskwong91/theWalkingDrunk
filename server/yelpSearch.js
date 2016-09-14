a3465e672f1353f24948ae65ab55befe9c6f52da
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-14 12:01:51 -0700

    Reimplmented the search module

--- server/yelpSearch.js
+++ server/yelpSearch.js
@@ -1,55 +1,44 @@
 var yelp = require("node-yelp");
-var db = require('./database/database.js');
-var fs = require('fs');
 
-var client = yelp.createClient({
-  oauth: {
-    "consumer_key": "_Qs9S9ohkGj32ygCz0vhyA",
-    "consumer_secret": "qA3YVgM_Z31LVHSV9qRLxtriboE",
-    "token": "VWLlZeJqo7anSjDjWe4i9N0SwuItniCZ",
-    "token_secret": "R21cba_B2dJmoElanqq1Wb1GWAQ"
-  },
+
+/**
+ * @name searchYelp
+ * @desc Given a request and a result, send the top 20 requests to the user. 
+ * @param {Request} req - A request
+ * @param {Result} res - A response
+ * @return {undefined} There is no defined return type.
+ */
+var searchYelp = function(req, res) {
+  // I'm not sure what parameters the query will have.
+  // if (req.query.NAME)
+    
+  var client = yelp.createClient({
+    oauth: {
+      'consumer_key': '_Qs9S9ohkGj32ygCz0vhyA',
+      'consumer_secret': 'qA3YVgM_Z31LVHSV9qRLxtriboE',
+      'token': 'VWLlZeJqo7anSjDjWe4i9N0SwuItniCZ',
+      'token_secret': 'R21cba_B2dJmoElanqq1Wb1GWAQ'
+    },
   
-  // Optional settings: 
-  httpClient: {
-    maxSockets: 25  // ~> Default is 10 
-  }
-});
+    // Optional settings: 
+    httpClient: {
+      maxSockets: 25  // ~> Default is 10 
+    }
+  });
+
+  client.search({
+    limit: 20,
+    category_filter: 'bars,nightlife',
+    location: '944 Market Street, San Francisco, CA 94102'
+  }).then(result => {
+    res.status(200).send(result);
+  }).catch(err => {
+    console.log(`There was en error: ${err}`);
+    res.status(400).send('There was an error');
+  });
+};
  
  
 // exports promise
-module.exports = (req, res) => {
-  // use memoization via prev. cached results
-  // ex.
-  // if (db.hasEntry(req.headers.bar)){
-  //   res.send(db.retrieveEntry(req.headers.bar));
-  //   return;
-  // }
-  // console.log(req.query.name, req.query.location);
-  console.log(req.query.location, '', 'req.query.loc');
-	client.search({
-	  terms: 'chilis',
-	  location: req.query.location + 'California, USA' ,
-     category_filter: "mexican"
-	})
-  .catch((err) => {
-    res.send(err);
-  })
-  .then((obj) => {
-    // does not check validity of result
-    // relies on accuracy of google.maps.places results
-    // console.log(obj.businesses[0]);
-    if (obj){
-      db.cacheYelpInfo(obj.businesses[0]); 
-      for (var i = 0; i < obj.businesses.length; i++){
-        console.log(obj.businesses[i].name);
-        if(obj.businesses[i].name === req.query.name){
-          res.send(obj.businesses[i]);
-          console.log('selected ', obj.businesses[i].name, obj.businesses[i].location);
-          return;
-        }
-      }
-    res.send(!!obj.businesses ? obj.businesses[0] : 'Try Again');
-    }
-  });
-};
+module.exports = searchYelp;
+
