bf26e2a2aeebea6fcf7f62961a5858c8f40b9625
Christopher Wong <chriskwong91@gmail.com>
2016-09-15 12:55:09 -0700

    Added the facebook login button

--- client/app/signup.jsx
+++ client/app/signup.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
 let signup = () => {
+  /* ENABLES FACEBOOK SDK */
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1059720117468553',
+      xfbml      : true,
+      version    : 'v2.7'
+      });
+    };
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  var facebookLogin = () => {
+    document.location.href='/auth/facebook';
+  };
+  // <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
   return (
-    <div>
-      <a href="/#/location">Signin</a>
+    <div className='container'>
+      <div className='row'>
+        <div className='col s8 offset-s2'>
+          <span className>Welcome to YelpCrawl!</span>
+          <div><span className='fb-login-button'>Sign in with FaceBook</span></div>
+        </div>
+      </div>
     </div>
   );
 };
