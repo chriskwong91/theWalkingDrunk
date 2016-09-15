cb9ad15c265e98bac88d227cac835daca597fa67
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-14 20:17:28 -0700

    (fix) Can now get user location

--- client/app/location.jsx
+++ client/app/location.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 let location = props => {
+  /**
+   * @name getCurrLoc
+   */
+  var getCurrLoc = function() {
+    navigator.geolocation.getCurrentPosition((res) => {
+      props.setLocation('' + res.coords.latitude + ',' + res.coords.longitude);
+    });
+  };
+  
   return (
     <div>
-      <input type="text"/>
+      <input name="userLocation" type="text"/>
       <a href="/#/decision">Add</a>
+      <br/>
+      <a href="/#/decision" onClick={getCurrLoc}>Use Current Location</a>
     </div>
   );
 };
