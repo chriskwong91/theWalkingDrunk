53a4b1fe81641106a130af5024d380020db7c5b3
Christopher Wong <chriskwong91@gmail.com>
2016-09-15 16:49:01 -0700

    style change for randomBar

--- client/app/randomBar.jsx
+++ client/app/randomBar.jsx
@@ -4,14 +4,16 @@ var randomBar = (props) => {
   console.log(props.randomBar);
   if (props.randomBar === '') {
     return (
-      <div className="preloader-wrapper active">
-        <div className="spinner-layer spinner-blue-only center-align">
-          <div className="circle-clipper left">
-            <div className="circle"></div>
-          </div><div className="gap-patch">
-            <div className="circle"></div>
-          </div><div className="circle-clipper right">
-            <div className="circle"></div>
+      <div className='spinner container center-align'>
+        <div className="preloader-wrapper active center-align">
+          <div className="spinner-layer spinner-blue-only center-align">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div><div className="gap-patch">
+              <div className="circle"></div>
+            </div><div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
           </div>
         </div>
       </div>
