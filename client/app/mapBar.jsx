89ca9cddd8550781859500692b5d21f3b3df0947
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-15 14:38:46 -0700

    Got basic map to render

--- client/app/mapBar.jsx
+++ client/app/mapBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Map from 'google-maps-react'
 
 
 var mapBar = (props) => {
   return (
     <div>
       Hi.
+      <Map google={window.google} />
     </div>
   );
 };
