c17bb5667cfafee844b5ae310d11603507eb110b
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-14 19:07:24 -0700

    (fix) Added basic location

---
+++ client/app/location.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let location = () => {
+  return (
+    <div>
+      <input type="text"/>
+      <button>Add</button>
+    </div>
+  );
+};
+
+export default location;
