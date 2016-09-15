138edd6ef64cefb7965da5a8e0ccb6cae5f713ee
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-15 14:52:20 -0700

    Started rendering markers

--- client/app/mapBar.jsx
+++ client/app/mapBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Map from 'google-maps-react'
+import {Map, Marker} from 'google-maps-react'
 
 
 var mapBar = (props) => {
+  console.log(props.bars);
   return (
     <div>
-      Hi.
-      <Map google={window.google} />
+      <Map google={window.google}>
+	<Marker
+	    name={'SOMA'}
+	    position={{lat: 37.778519, lng: -122.405640}} />
+      </Map>
     </div>
   );
 };
