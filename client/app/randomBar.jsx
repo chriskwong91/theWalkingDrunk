5486ceab5c1895b3f58bde3a4342b22839ddac20
Christopher Wong <chriskwong91@gmail.com>
2016-09-15 16:36:53 -0700

    Set up of randombar to have a spinner waiting for the return

--- client/app/randomBar.jsx
+++ client/app/randomBar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 var randomBar = (props) => {
-  return (
-    <div>
-      {props.randomBar}
-    </div>
-  );
+  console.log(props.randomBar);
+  if (props.randomBar === '') {
+    return (
+      <div className="preloader-wrapper active">
+        <div className="spinner-layer spinner-blue-only center-align">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div><div className="gap-patch">
+            <div className="circle"></div>
+          </div><div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+      Hi
+      </div>
+    );
+  }
 };
 
 export default randomBar;
