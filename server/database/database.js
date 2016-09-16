2ff9fe673983ab4c890b2e217be5a52a2fe116d0
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-15 20:51:38 -0700

    Started a server connection

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,31 @@
 var mysql = require('promise-mysql');
+var config = require('../config/env/config.js');
 var connection;
+
+
+/**
+ * @name createConn
+ * @desc Given a connection variable, create a connection to our database. 
+ * @param {var} conn -
+ * @return {undefined}
+ */
+var createConn = function(conn) {
+  mysql.createConnection(config.sql)
+    .then(res => {
+      conn = res;
+    })
+    .catch(err => {
+      console.log('There was an error with the server, attempting to reconnect in 10 seconds.');
+      console.error(err);
+      setTimeout(createConn(conn), 1000*10);
+    });
+};
+
+// Create a connection to our server when the server starts.
+// For information on escaping using nodesql, refer to:
+//   https://github.com/mysqljs/mysql#escaping-query-values
+createConn(connection);
+
 
 
 
