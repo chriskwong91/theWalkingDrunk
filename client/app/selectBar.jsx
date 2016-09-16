7bc76c1a733d6e7cf213ac343e744bdd47837273
Brian Zhou <bzhou4@gmail.com>
2016-09-16 12:50:22 -0700

    (fix) changed the logic for the maps button on the page

--- client/app/selectBar.jsx
+++ client/app/selectBar.jsx
@@ -17,41 +17,77 @@ var selectBar = (props) => {
             } 
           }>Add</button>
 
+
+  spinnig bar:
+  () => {
+        <div className='spinner container center-align'>
+          <div className="preloader-wrapper active center-align">
+            <div className="spinner-layer spinner-blue-only center-align">
+              <div className="circle-clipper left">
+                <div className="circle"></div>
+              </div><div className="gap-patch">
+                <div className="circle"></div>
+              </div><div className="circle-clipper right">
+                <div className="circle"></div>
+              </div>
+            </div>
+          </div>
+        </div> }
   */
+
+  //  <div href="/#/mapbar" className="select-go-to-map center-align thin">
+  //   <a href="/#/mapbar" className="to-map">Kickoff My yelpCrawl!</a>
+  // </div>
 
   var bars = bars.slice(0,10);
 
   return (
     <div>
-      <p className="h1-select center-align thin">Select Bars</p>
-
-   		{bars.length === 0 ? 'loading...' : bars.map( (bar, index) =>
-   			<div key={ index }>
-          <div className = "select-bar-info thin center-align">
-     			  <h5>{ bar.name }</h5>
-     			  <span>{ bar.location.address }</span><br/>
-     			  <span>{ bar.location.city }</span><br/>
-     			  <span>Rating: { bar.rating }</span><br/>
-     			  <span>Review Count: { bar.review_count }</span>
-     			  <p>{ bar.snippet_text }</p>
-          </div>
-   			  
-          <div className="select-bar-test thin center-align" onClick={ () => { 
-              props.addWaypoint( bars[index] )
-            } 
-          }>Add <b>{bar.name}</b> to your Route!</div>
-   			</div>
-   		)}
-
-      <div href="/#/mapbar" className="select-go-to-map center-align thin">
-        <a href="/#/mapbar" className="to-map">Kickoff My yelpCrawl!</a>
+      <div className = "h1-header">
+        <p className="h1-select center-align thin">Select Bars</p>
       </div>
 
+   		{bars.length === 0 ? <div className='spinner container center-align'>
+          <div className="preloader-wrapper active center-align">
+            <div className="spinner-layer spinner-blue-only center-align">
+              <div className="circle-clipper left">
+                <div className="circle"></div>
+              </div><div className="gap-patch">
+                <div className="circle"></div>
+              </div><div className="circle-clipper right">
+                <div className="circle"></div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+      : bars.map( (bar, index) => 
+   			<div>
+          <div key={ index }>
+            <div className = "select-bar-info thin center-align">
+       			  <h5>{ bar.name }</h5>
+       			  <span>{ bar.location.address }</span><br/>
+       			  <span>{ bar.location.city }</span><br/>
+       			  <span>Rating: { bar.rating }</span><br/>
+       			  <span>Review Count: { bar.review_count }</span>
+       			  <p>{ bar.snippet_text }</p>
+            </div>
+     			  
+            <div className="select-bar-test thin center-align" onClick={ () => { 
+                props.addWaypoint( bars[index] )
+              } 
+            }>Add <b>{bar.name}</b> to your Route!</div>
+     			</div>
+          <span>{index === 9 ? 
+             <div href="/#/mapbar" className="select-go-to-map center-align thin">
+              <a href="/#/mapbar" className="to-map">Kickoff My yelpCrawl!</a>
+            </div>
+           : <div></div>}</span>
+        </div>
+      )
+    }
     </div>
-
-
-  );
-
+  )
 };
 
 export default selectBar;
