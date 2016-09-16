b8fcd6f9a18942828a920a82545da4a10c517ae1
Christopher Wong <chriskwong91@gmail.com>
2016-09-16 12:21:07 -0700

    started set up users on sql server

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,9 @@
 //This page builds allows the user to login with passport-facebook
 
 var FacebookStrategy = require('passport-facebook').Strategy;
-var User = require('../database/database.js').User;
-
+// var User = require('../database/database.js').User;
+var connection = require('../database/database.js').connection;
+var mysql = require('promise-mysql');
 //loads facebook or other auth variables
 // var configAuth = require('./auth');
 var configAuth = require('./env/config.js');
@@ -39,30 +40,52 @@ module.exports = function(passport) {
     //async process
     process.nextTick(() => {
       //finds if user is in database
-      User.findOne({ 'facebook.id' : profile.id }, (err, user) => {
-        if (err) { return done(err); }
-        // if found, return user
-          console.log(profile);
-        if (user) {
-          console.log(user);
-          return done(null, user);
+      var findUserQuery = `
+        select * from dev.user
+        where uid = ${mysql.escape(profile.id)};
+      `;
+      connection.findUserQuery().then(res => {
+        if (res) {
+          console.log('user exists: ', res);
         } else {
           //create new user
-          var newUser = new User();
-          newUser.facebook.id = profile.id;
-          newUser.facebook.token = token;
-          // newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyname;
-          newUser.facebook.name = profile.displayName;
-          newUser.facebook.email = profile.emails[0].value;
-          newUser.facebook.friends = profile.friends.data;
-          console.log(newUser);
-          newUser.save((err) => {
-            if (err) { throw err; }
-
-            return done(null, newUser);
+          var newUserQuery = `
+            insert into dev.users
+            (uid, name, email)
+            values
+            (${mysql.escape(profile.id)}, ${mysql.escape(name)}, ${mysql.escape(email)});
+          `;
+          connection.newUserQuery().then(res => {
+            console.log('New User added: ', res);
           });
         }
       });
+
+
+      // User.findOne({ 'facebook.id' : profile.id }, (err, user) => {
+      //   if (err) { return done(err); }
+      //   // if found, return user
+      //     console.log(profile);
+      //   if (user) {
+      //     console.log(user);
+      //     return done(null, user);
+      //   } else {
+      //     //create new user
+      //     var newUser = new User();
+      //     newUser.facebook.id = profile.id;
+      //     newUser.facebook.token = token;
+      //     // newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyname;
+      //     newUser.facebook.name = profile.displayName;
+      //     newUser.facebook.email = profile.emails[0].value;
+      //     newUser.facebook.friends = profile.friends.data;
+      //     console.log(newUser);
+      //     newUser.save((err) => {
+      //       if (err) { throw err; }
+
+      //       return done(null, newUser);
+      //     // });
+      //   }
+      // });
     });
   }));
 };
