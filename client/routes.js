f9e5fd4ba7ac2d50601aa64b7e925d231d40cadb
Christopher Wong <chriskwong91@gmail.com>
2016-09-16 18:37:27 -0700

    added browserhistory

--- client/routes.js
+++ client/routes.js
@@ -9,7 +9,7 @@ import selectBar from './app/selectBar.jsx';
 import signup from './app/signup.jsx';
 import RouteList from './app/RouteList.jsx';
 import Index from './app/index.jsx';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 if (window.location.hash == '#_=_'){
     history.replaceState
