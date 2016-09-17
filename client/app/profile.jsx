aa689caa28ef33c08b3afd567def7cda020d8ce7
Christopher Wong <chriskwong91@gmail.com>
2016-09-17 13:19:30 -0700

    tells user to sign in if not signed in to view profile

--- client/app/profile.jsx
+++ client/app/profile.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {browserHistory} from 'react-router';
 
 var profile = (props) => {
+  let handleSignIn = () => {
+    window.location.href = '/auth/facebook';
+  };
+
+  let handleStart = () => {
+    browserHistory.push('#/location');
+  };
 	return (
 		<div>
 		  <div className="location-h1 center-align">
         <h1 className="thin">Profile</h1>
+        {Object.keys(props.currentUser).length === 0 ?
+        <button onClick={handleSignIn} className='btn'>Sign In!</button> :
+        <div>
+          <h3 className="thin">Welcome {props.currentUser.name}!</h3>
+          <h4 className="thin">{props.currentUser.email}</h4>
+          <button onClick={handleStart} className='btn'>Start the Journey!</button>
+        </div>
+        }
       </div>
     </div>
 	);
