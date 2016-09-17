f105464c27fb95b6b11b5a0f921c286e31f967ed
Christopher Wong <chriskwong91@gmail.com>
2016-09-17 00:00:15 -0700

    working friend list and friend entry list views with no functionality

--- client/app/FriendEntryList.jsx
+++ client/app/FriendEntryList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-let FriendEntryList = () => (
-  <div>
-    EntryList Hi
-  </div>
-);
+let FriendEntryList = () => {
+  return (
+    <div>
+      EntryList Hi
+    </div>
+  )
+}
 
-export default FriendEntryList.jsx;
+export default FriendEntryList;
