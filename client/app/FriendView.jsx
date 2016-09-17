82b4c1f7484b8b68cf962d0a0ba5d94101a6822a
Christopher Wong <chriskwong91@gmail.com>
2016-09-16 23:51:53 -0700

    imported friendentrylist

--- client/app/FriendView.jsx
+++ client/app/FriendView.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import FriendEntryList from './FriendEntryList.jsx';
 
 let FriendView = props => {
   return (
     <div>
+      <h1>Yelp Crawlin Friends</h1>
+        <FriendEntryList />
     </div>
   )
 }
